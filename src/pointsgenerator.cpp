79f275605a3c7270d386f0812e26dde9c0153636
voganesyan <oganesyan@cvisionlab.com>
2022-11-13 13:50:55 +0300

    moved thread's sleep to the end of the loop to avoid delay on the first iteration

--- src/pointsgenerator.cpp
+++ src/pointsgenerator.cpp
@@ -9,20 +9,19 @@ static const int SLEEP_TIME_MS = 500;
 void PointsGenerator::run()
 {
     while (true) {
-        msleep(SLEEP_TIME_MS);
         if (isInterruptionRequested()) {
             break;
         }
-        if (is_paused) {
-            continue;
+        if (!is_paused) {
+            QList<QPointF> points;
+            for (int i = 0; i < LIST_SIZE; i++) {
+                int x = std::rand() % max_value;
+                int y = std::rand() % max_value;
+                points << QPointF(x, y);
+            }
+            emit generated(points);
         }
-        QList<QPointF> points;
-        for (int i = 0; i < LIST_SIZE; i++) {
-            int x = std::rand() % max_value;
-            int y = std::rand() % max_value;
-            points << QPointF(x, y);
-        }
-        emit generated(points);
+        msleep(SLEEP_TIME_MS);
     }
     is_paused = false;
     qDebug() << "Finish";
